d3759818eba459be5111ea5efcef32f43d86e274
NguyentDuyit <nguyentduyit@gmail.com>
2025-02-10 09:25:14 +0700

    update form-react

--- src/components/MiniAppForm.jsx
+++ src/components/MiniAppForm.jsx
@@ -2,9 +2,8 @@ import React, { useState } from 'react'
 import clsx from 'clsx';
 
 function MiniAppForm({ handleSubmit }) {
-    const [isSubmit, setIsSubmit] = React.useState(false);
+    const [isSubmit, setIsSubmit] = useState(false);
     const [inputs, setInputs] = useState({
-        key: Date.now(),
         name: "",
         email: "",
         password: "",
@@ -14,27 +13,15 @@ function MiniAppForm({ handleSubmit }) {
     })
     const [errors, setErrors] = useState({})
 
-    function onChangeInput(e, type) {
-        const { name, value, checked } = e.target;
-        if(type === 'checkbox' && !checked) {
-            setInputs(prevState => {
-                return {
-                    ...prevState,
-                    gender: ''
-                }
-            })
-        } else {
-            setInputs((prevState) => {
-                return {
-                    ...prevState,
-                    [name]: value,
-                }
-            })
-        }
+    function onChangeInput(e) {
+        const { name, value } = e.target;
+        setInputs((prevState) => {
+            return {
+                ...prevState,
+                [name]: value,
+            }
+        })
     }
-
-    console.log('inputs', {errors, inputs})
-
     const _handleSubmit = (e) => {
         e.preventDefault()
         let errorsSubmit = {}
@@ -79,7 +66,7 @@ function MiniAppForm({ handleSubmit }) {
     return (
         <>
             <form type="sumit">
-                <div className='mb-6' >
+                <div className='mb-6 w-3/6 m-auto' >
                     <label className={clsx(
                         'block mb-2 text-sm font-medium ',
                         isSubmit && errors.name && 'text-red-700 dark:text-red-500'
@@ -97,88 +84,113 @@ function MiniAppForm({ handleSubmit }) {
                         {errors.name && <p className="mt-2 text-sm text-red-600 dark:text-red-500">Input Name!</p>}
                     </label>
                 </div>
-                <div className='mb-6' >
-                    <label className='block mb-2 text-sm font-medium text-red-700 dark:text-red-500'>
-                        Email Address
-                        <input className='bg-red-50 border border-red-500 text-red-900 placeholder-red-700 text-sm rounded-lg focus:ring-red-500 block w-full p-2.5'
+                <div className='mb-6 w-3/6 m-auto' >
+                    <label className={clsx(
+                        'block mb-2 text-sm font-medium ',
+                        isSubmit && errors.email && 'text-red-700 dark:text-red-500'
+                    )}>
+                        Email
+                        <input
+                            className={clsx(
+                                'border block w-full p-2.5',
+                                isSubmit && errors.email && 'bg-red-50 border-red-500 text-red-900 placeholder-red-700 text-sm rounded-lg focus:ring-red-500'
+                            )}
                             name='email'
-                            type='email'
                             onChange={(e) => onChangeInput(e, 'input')}
                         >
                         </input>
-                        {errors.email && <p className="mt-2 text-sm text-red-600 dark:text-red-500">{errors.email}.</p>}
+                        {errors.email && <p className="mt-2 text-sm text-red-600 dark:text-red-500">Input Email!</p>}
                     </label>
                 </div>
-                <div className='mb-6' >
-                    <label className='block mb-2 text-sm font-medium text-red-700 dark:text-red-500'>
+                <div className='mb-6 w-3/6 m-auto' >
+                    <label className={clsx(
+                        'block mb-2 text-sm font-medium ',
+                        isSubmit && errors.country && 'text-red-700 dark:text-red-500'
+                    )}>
                         Country
-                        <input className='bg-red-50 border border-red-500 text-red-900 placeholder-red-700 text-sm rounded-lg focus:ring-red-500 block w-full p-2.5'
+                        <input
+                            className={clsx(
+                                'border block w-full p-2.5',
+                                isSubmit && errors.country && 'bg-red-50 border-red-500 text-red-900 placeholder-red-700 text-sm rounded-lg focus:ring-red-500'
+                            )}
                             name='country'
                             onChange={(e) => onChangeInput(e, 'input')}
                         >
                         </input>
-                        {errors.country && <p className="mt-2 text-sm text-red-600 dark:text-red-500">{errors.country}</p>}
+                        {errors.country && <p className="mt-2 text-sm text-red-600 dark:text-red-500">Input Country!</p>}
                     </label>
                 </div>
-                <div className='mb-6'>
-                    <label className='block mb-2 text-sm font-medium text-red-700 dark:text-red-500'>
+                <div className='mb-6 w-3/6 m-auto'>
+                    <label className={clsx(
+                        'block mb-2 text-sm font-medium ',
+                        isSubmit && errors.password && 'text-red-700 dark:text-red-500'
+                    )}>
                         Password
-                        <input className='bg-red-50 border border-red-500 text-red-900 placeholder-red-700 text-sm rounded-lg focus:ring-red-500 block w-full p-2.5'
-                            name="password"
+                        <input
+                            className={clsx(
+                                'border block w-full p-2.5',
+                                isSubmit && errors.password && 'bg-red-50 border-red-500 text-red-900 placeholder-red-700 text-sm rounded-lg focus:ring-red-500'
+                            )}
+                            name='password'
                             type='password'
                             onChange={(e) => onChangeInput(e, 'input')}
                         >
                         </input>
-                        {errors.password && <p className="mt-2 text-sm text-red-600 dark:text-red-500">{errors.password}</p>}
+                        {errors.password && <p className="mt-2 text-sm text-red-600 dark:text-red-500">Input Password!</p>}
                     </label>
                 </div>
-                <div className='mb-6' >
-                    <label className='block mb-2 text-sm font-medium text-red-700 dark:text-red-500'>
-                        Confirm password
-                        <input className='bg-red-50 border border-red-500 text-red-900 placeholder-red-700 text-sm rounded-lg focus:ring-red-500 block w-full p-2.5'
+                <div className='mb-6 w-3/6 m-auto' >
+                    <label className={clsx(
+                        'block mb-2 text-sm font-medium ',
+                        isSubmit && errors.repassword && 'text-red-700 dark:text-red-500'
+                    )}>
+                        Repassword
+                        <input
+                            className={clsx(
+                                'border block w-full p-2.5',
+                                isSubmit && errors.repassword && 'bg-red-50 border-red-500 text-red-900 placeholder-red-700 text-sm rounded-lg focus:ring-red-500'
+                            )}
                             name='repassword'
                             type='password'
                             onChange={(e) => onChangeInput(e, 'input')}
                         >
                         </input>
-                        {errors.repassword && <p className="mt-2 text-sm text-red-600 dark:text-red-500">{errors.repassword}</p>}
+                        {errors.repassword && <p className="mt-2 text-sm text-red-600 dark:text-red-500">Input Repassword!</p>}
                     </label>
                 </div>
-                <div className='mb-6' >
-                    <label className='block mb-2 text-sm font-medium text-red-700 dark:text-red-500'>
+                <div className='mb-6 w-3/6 m-auto' >
+                    <label className={clsx(isSubmit && errors.repassword &&
+                        'block mb-2 text-sm font-medium text-red-700 dark:text-red-500')}>
                         Gender
                         <div>
-                            <div>
-                                <label
-                                >Male</label>
-                                <input
-                                    className='gender-check p-3 ml-2'
-                                    name="gender"
-                                    value="Male"
-                                    type='checkbox'
-                                    checked={inputs.gender === "Male"}
-                                    onChange={(e) => onChangeInput(e, 'checkbox')}
-                                >
-                                </input>
-                            </div>
-                            <div>
-                                <label
-                                >
-                                    Female
-                                </label>
-                                <input
-                                    type='checkbox'
-                                    name="gender"
-                                    className='gender-check p-3 ml-2'
-                                    value="Female"
-                                    checked={inputs.gender === "Female"}
-                                    onChange={(e) => onChangeInput(e, 'checkbox')}
-                                >
-                                </input>
-                            </div>
+                            <label
+                            >Male</label>
+                            <input
+                                className='gender-check p-3 ml-2'
+                                name="gender"
+                                value="Male"
+                                type='radio'
+
+                                onChange={(e) => onChangeInput(e)}
+                            >
+                            </input>
+                        </div>
+                        <div>
+                            <label
+                            >
+                                Female
+                            </label>
+                            <input
+                                type='radio'
+                                name="gender"
+                                className='gender-check p-3 ml-2'
+                                value="Female"
+                                onChange={(e) => onChangeInput(e)}
+                            >
+                            </input>
                         </div>
                     </label>
-                    {errors.gender && <p className="mt-2 text-sm text-red-600 dark:text-red-500">{errors.gender}</p>}
+                    {errors.gender && <p className="mt-2 text-sm text-red-600 dark:text-red-500">Choose gender</p>}
                 </div>
                 <button className="bg-blue-700 text-white font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center"
                     onClick={_handleSubmit}>
